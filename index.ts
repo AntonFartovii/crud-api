782cf37cb1bc703f55848c2fa647b6d3327a5556
AntonFartovii <anton@rza.by>
2023-03-06 17:09:51 +0300

    init: ts tsconfig.json package.json

--- index.ts
+++ index.ts
@@ -1,18 +1,31 @@
-import http from 'http'
+import http from 'node:http'
+import cluster from 'node:cluster'
+import process from 'node:process'
+import { cpus } from 'node:os'
+
 import 'dotenv/config'
-import {App} from "./src/app";
+import {App} from "./src/app"
 
-
+const countCPUs = cpus().length
 const PORT: number = Number( process.env.PORT )
-
-
-
-
 const app: any = new App()
 
+if ( cluster.isPrimary ) {
+    console.log(`Primary ${process.pid} is running`);
+
+    for (let i = 0; i < countCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    app.listen(PORT, () => {
+        // console.log( `Server running on port ${PORT}`)
+        console.log(`Worker ${process.pid} started`);
+    })
+}
 
 
 
-app.listen(PORT, () => {
-    console.log( `Server running on port ${PORT}`)
-})
